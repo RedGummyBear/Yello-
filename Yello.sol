3f624c875a22f6c7de41996402a7173e1158a691
RedGummyBear <151572775+RedGummyBear@users.noreply.github.com>
2023-12-17 17:45:26 +0300

    Create Yello.sol

---
+++ Yello.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.5.0;
+
+contract Yello {
+    function greet() public pure returns (string memory) {
+        return "yello! :)";
+    }
+}
